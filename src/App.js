71c5c6c01ff800fdd9e6b8e1e3826efe2d2452e2
Justin <justinlcochran@gmail.com>
2023-05-29 09:58:59 -0600

    working on env and cognito

--- src/App.js
+++ src/App.js
@@ -1,35 +1,116 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import { Auth } from "aws-amplify";
 import './App.css';
-
+import {Amplify} from "aws-amplify";
+import awsconfig from "./aws-exports";
+import {Authenticator, Heading, Text, useTheme, withAuthenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import Home from "./pages/Home";
 import Flinn from "./pages/Flinn";
 import York from "./pages/York";
 
-function App() {
-    Auth.currentSession()
-        .then(data => {
-            const accessToken = data.getAccessToken().getJwtToken();
-            // Use the access token as needed
-        })
-        .catch(error => {
-            // Handle any errors
-            console.log(error, "got it")
-        });
+Amplify.configure(awsconfig);
 
-    console.log(process.env.AWS_APP_ID, "env")
+const formFields = {
+    confirmVerifyUser: {
+        confirmation_code: {
+            label: 'New Label',
+            placeholder: 'Enter your Confirmation Code:',
+            isRequired: false,
+        },
+    },
+};
 
-  return (
-      <div className="App h-screen">
-        <BrowserRouter>
-          <Routes>
-            <Route exact path='/prep' element={<Home />} />
-            <Route exact path='/flinn' element={<Flinn />} />
-            <Route exact path='/york' element={<York />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-  );
+const components = {
+    VerifyUser: {
+        Header() {
+            const { tokens } = useTheme();
+            return (
+                <Heading
+                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+                    level={3}
+                >
+                    Enter Information:
+                </Heading>
+            );
+        },
+        Footer() {
+            return <Text>Footer Information</Text>;
+        },
+    },
+
+    ConfirmVerifyUser: {
+        Header() {
+            const { tokens } = useTheme();
+            return (
+                <Heading
+                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+                    level={3}
+                >
+                    Enter Information:
+                </Heading>
+            );
+        },
+        Footer() {
+            return <Text>Footer Information</Text>;
+        },
+    },
+};
+
+export default function App() {
+    return (<>
+        <Authenticator
+            formFields={formFields}
+            components={components}
+            hideSignUp={true}
+        >
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user.attributes.name}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route exact path='/prep' element={<Home />} />
+                            <Route exact path='/flinn' element={<Flinn />} />
+                            <Route exact path='/york' element={<York />} />
+                        </Routes>
+                    </BrowserRouter>
+                </main>
+            )}
+        </Authenticator>
+
+        </>
+    );
 }
 
-export default App;
+// function App() {
+//     Auth.currentSession()
+//         .then(data => {
+//             const accessToken = data.getAccessToken().getJwtToken();
+//             // Use the access token as needed
+//         })
+//         .catch(error => {
+//             // Handle any errors
+//             console.log(error, "got it")
+//         });
+//
+//     console.log(process.env.AWS_APP_ID, "env")
+//
+//   return (
+//       <div className="App h-screen">
+//           <Authenticator
+//               hideSignUp={true}
+//           >
+//               {({ signOut, user }) => (
+//                   <main>
+//                       <h1>Hello {user.username}</h1>
+//                       <button onClick={signOut}>Sign out</button>
+//                   </main>
+//               )}
+//           </Authenticator>
+
+//       </div>
+//   );
+// }
+
+// export default withAuthenticator(App);
